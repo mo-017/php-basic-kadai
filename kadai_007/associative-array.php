9e44dc0d4631affefdadadd11200ac1cc95db673
mo-017 <work.momoe17@gmail.com>
2025-05-14 07:18:49 +0900

    課題007追加

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP第7章課題</title>
+</head>
+<body>
+  <p>
+    <?php
+    // 連想配列に値を代入する
+    $commodity = ['name' => 'onion', 'price' => 200 , 'weight' => 160 ];
+
+    // 連想配列の値を出力する
+    print_r($commodity);
+
+    ?>
+    </p>   
+    
+</body>
+</html>
